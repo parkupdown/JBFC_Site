d2ce326750f9f1050148199705a51362ff9c9ce5
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-27 19:22:55 +0900

    feat: 회원가입 성공 시 가입 멘트 출력 및 loginpage로 이동

--- jbfc/src/routes/Sign.js
+++ jbfc/src/routes/Sign.js
@@ -1,19 +1,17 @@
 import axios from "axios";
 
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Sign() {
   const [userId, setUserId] = useState(``);
   const [password, setPassword] = useState();
-  let pass = false;
+  const navigate = useNavigate();
 
   const onCheck = (event) => {
-    pass = false;
     event.preventDefault();
     setUserId(event.currentTarget[0].value);
     setPassword(event.currentTarget[1].value);
-    event.currentTarget[0].value = ``;
-    event.currentTarget[1].value = ``;
   };
 
   useEffect(() => {
@@ -24,20 +22,24 @@ function Sign() {
         usePass: password,
       })
       .then((res) => alert(res.data));
-
-    pass = true;
   }, [userId]);
 
   const onSubmit = (event) => {
     event.preventDefault();
-    axios.post(`http://localhost:8080/sign/insertData`, {
-      name: "짝발란스",
-      useId: userId,
-      usePass: password,
-    });
+    axios
+      .post(`http://localhost:8080/sign/insertData`, {
+        name: "짝발란스",
+        useId: userId,
+        usePass: password,
+      })
+      .then(goToMain())
+      .then(alert(`회원가입이 완료되었습니다.`));
     //이제 여기서 이동 usenavigator로!
   };
 
+  const goToMain = () => {
+    navigate(`/`);
+  };
   return (
     <div>
       <h1>회원가입 페이지 입니다.</h1>
@@ -46,7 +48,7 @@ function Sign() {
         <input placeholder="Password" />
         <button>체크!</button>
       </form>
-      <form onSubmit={onSubmit} name="id" disabled={pass}>
+      <form onSubmit={onSubmit} name="id">
         <button>제출</button>
       </form>
     </div>
