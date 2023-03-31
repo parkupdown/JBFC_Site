cd00e5c01f9ebaf15ada846de8dd782d58bc3d6f
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-31 15:36:57 +0900

    refactor: 미세먼지,날씨 정보를 Home 컴포넌트에서 한번에 받아와서 props로 하위 컴포넌트에 보내도록 변경

--- jbfc/src/routes/Pollution.js
+++ jbfc/src/routes/Pollution.js
@@ -1,8 +1,3 @@
-import { useQuery } from "react-query";
-import axios from "axios";
-import { PollutionInfo } from "../atoms";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-
 function UnixToDate(Unix) {
   const time = new Date(Unix * 1000);
   const week = [`일`, `월`, `화`, `수`, `목`, `금`, `토`];
@@ -12,36 +7,9 @@ function UnixToDate(Unix) {
   }월 ${time.getDate()}일 ${time.getHours()}시 ${dayOfWeek}요일`;
 } //옮겨줘여함
 
-function Pollution() {
-  const getLocation = () => {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition((position) => {
-        resolve(position);
-      });
-    });
-  };
-  const callApi = (lat, lnd) => {
-    return axios
-      .get(
-        `http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat}&lon=${lnd}&appid=2834387742b25d5393a21e88fee8246a`
-      )
-      .then((res) => res);
-  };
-
-  const getPollution = async () => {
-    const latlnd = await getLocation();
-    const data = await callApi(latlnd.coords.latitude, latlnd.coords.longitude);
-
-    return data;
-  };
-  const { isLoading, data } = useQuery("Pollution", getPollution);
-
-  const setPollution = useSetRecoilState(PollutionInfo);
-  setPollution(data);
-
+function Pollution({ data }) {
   const makePollutionInfo = (data) => {
     let pollutionInfo = data.data.list;
-    const timeInfo = pollutionInfo.map((item) => UnixToDate(item.dt));
     //이건 나중에 자세한 예보를 보여줄 때 사용하면 좋을 거 같다.
     const pm10 = pollutionInfo[0].components.pm10;
     const pm25 = pollutionInfo[0].components.pm2_5;
@@ -49,24 +17,14 @@ function Pollution() {
     return [pm10, pm25, day];
   };
 
-  if (isLoading === false) {
-    makePollutionInfo(data);
-  }
-
   return (
     <div>
-      {isLoading ? (
-        "로딩중입니당"
-      ) : (
-        <div>
-          <h3>실시간 미세먼지 정보!</h3>
-          <ul>
-            <li>초미세먼지: {makePollutionInfo(data)[0]}</li>
-            <li>미세먼지: {makePollutionInfo(data)[1]}</li>
-            <li>기준날짜: {makePollutionInfo(data)[2]}</li>
-          </ul>
-        </div>
-      )}
+      <h3>실시간 미세먼지 정보!</h3>
+      <ul>
+        <li>초미세먼지: {makePollutionInfo(data)[0]}</li>
+        <li>미세먼지: {makePollutionInfo(data)[1]}</li>
+        <li>기준날짜: {makePollutionInfo(data)[2]}</li>
+      </ul>
     </div>
   );
 }
