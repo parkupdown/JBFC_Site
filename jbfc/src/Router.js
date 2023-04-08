2cbe3f98644695723b1771edae60229509cf2681
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-04-08 10:14:02 +0900

    refactor: 라우트 구조를 변경하여 UI 개선

--- jbfc/src/Router.js
+++ jbfc/src/Router.js
@@ -1,5 +1,8 @@
 import Board from "./routes/Board";
 import BoardDetail from "./routes/BoardDetail";
+import BoardMine from "./routes/BoardMine";
+import Chat from "./routes/Chat";
+import DetailWeatherPollution from "./routes/DetailWeatherPollution";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
 import Sign from "./routes/Sign";
@@ -12,9 +15,15 @@ function Router() {
       <Routes>
         <Route path="/" element={<Login />}></Route>
         <Route path="/sign" element={<Sign />}></Route>
-        <Route path="/home/*" element={<Home />}></Route>
+        <Route path="/home/" element={<Home />}></Route>
         <Route path="/board" element={<Board />}></Route>
         <Route path="/board/:userId" element={<BoardDetail />}></Route>
+        <Route path="/board/mine" element={<BoardMine />}></Route>
+        <Route path="/chat/*" element={<Chat />}></Route>
+        <Route
+          path="/WeatherPollution/*"
+          element={<DetailWeatherPollution />}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
