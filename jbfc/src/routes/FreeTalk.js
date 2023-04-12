4fcb499f96018ce2bf0d710fa6dc929dcdef1349
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-04-12 21:21:04 +0900

    design: 채팅컴포넌트를 styled-components를 이용해 css 추가

--- jbfc/src/routes/FreeTalk.js
+++ jbfc/src/routes/FreeTalk.js
@@ -1,5 +1,94 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #f2f2f2;
+  color: #333;
+`;
+
+const ChatContainer = styled.div`
+  margin-top: 0px;
+  height: 650px;
+  overflow-y: scroll;
+  flex-direction: column-reverse;
+  display: flex;
+`;
+
+const ScrollBar = styled.div`
+  width: 20px;
+  background-color: #f2f2f2;
+  border-radius: 10px;
+  margin: 5px;
+`;
+
+const Thumb = styled.div`
+  width: 10px;
+  background-color: #555;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+const Track = styled.div`
+  width: 10px;
+  background-color: #f2f2f2;
+  border-radius: 10px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputContainer = styled.form`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+
+  input {
+    flex: 1;
+    margin-right: 1rem;
+    padding: 0.5rem;
+    border-radius: 4px;
+    border: none;
+  }
+
+  button {
+    background-color: #03a9f4;
+    color: #fff;
+    padding: 0.5rem;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #01579b;
+    }
+  }
+`;
+
+const ChatList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    margin-left: 1rem;
+    margin-bottom: 0.5rem;
+    align-items: end;
+  }
+`;
+
+const Ment = styled.h1`
+  margin-top: 30px;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  color: #0288d1;
+  text-align: center;
+`;
 
 function FreeTalk({ userId, socket, data }) {
   const [chatData, setChatData] = useState(null);
@@ -24,7 +113,7 @@ function FreeTalk({ userId, socket, data }) {
   };
   const emitMessage = (ChatValue) => {
     return new Promise((resolve, reject) => {
-      socket.emit("send_message_free", { message: ChatValue });
+      socket.emit("send_message_free", { userId: userId, message: ChatValue });
     });
   };
 
@@ -36,46 +125,34 @@ function FreeTalk({ userId, socket, data }) {
     event.currentTarget[0].value = ``;
   };
 
-  if (data !== null) {
-    const Ul = document.querySelector("ul");
-    console.log(Ul);
-    const li = document.createElement("li");
-    Ul.appendChild(li);
-    li.innerHTML = `${data.userId}: ${data.message}`;
-  }
+  // 이게 아니라 setState에 들어갔던 것이 나오도록해야할듯
+  //이부분이 잘못됨
 
   return (
-    <div>
-      <h2>자유톡📢</h2>
+    <Container>
       {chatData === null ? (
         <div>
           <h3>"로딩중"</h3>
-          <ul></ul>
+          <ChatList></ChatList>
         </div>
       ) : (
-        <div
-          style={{
-            width: "200px",
-            height: "300px",
-            overflow: "auto",
-            flexDirection: "column-reverse",
-            display: "flex",
-          }}
-        >
-          <form onSubmit={sendMessage}>
+        <ChatContainer>
+          <InputContainer onSubmit={sendMessage}>
             <input placeholder="채팅" />
             <button>보내기</button>
-          </form>
-          <ul>
+          </InputContainer>
+
+          <ChatList>
             {chatData === null
               ? null
               : chatData.map((data, index) => (
                   <li key={index}>{`${data.userId}: ${data.chatData}`}</li>
                 ))}
-          </ul>
-        </div>
+          </ChatList>
+        </ChatContainer>
       )}
-    </div>
+      <Ment>바른 채팅</Ment>
+    </Container>
   );
 }
 
--- jbfc/src/routes/NoticeTalk.js
+++ jbfc/src/routes/NoticeTalk.js
@@ -1,7 +1,96 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-function NoticeTalk({ userId, socket, data }) {
+const Container = styled.div`
+  background-color: #f2f2f2;
+  color: #333;
+  width: 100%;
+  height: 100%;
+`;
+
+const ChatContainer = styled.div`
+  margin-top: 0px;
+  height: 650px;
+  overflow-y: scroll;
+  flex-direction: column-reverse;
+  display: flex;
+`;
+
+const ScrollBar = styled.div`
+  width: 20px;
+  background-color: #f2f2f2;
+  border-radius: 10px;
+  margin: 5px;
+`;
+
+const Thumb = styled.div`
+  width: 10px;
+  background-color: #555;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+const Track = styled.div`
+  width: 10px;
+  background-color: #f2f2f2;
+  border-radius: 10px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputContainer = styled.form`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+
+  input {
+    flex: 1;
+    margin-right: 1rem;
+    padding: 0.5rem;
+    border-radius: 4px;
+    border: none;
+  }
+
+  button {
+    background-color: #03a9f4;
+    color: #fff;
+    padding: 0.5rem;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #01579b;
+    }
+  }
+`;
+
+const ChatList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    margin-left: 1rem;
+    margin-bottom: 0.5rem;
+    align-items: end;
+  }
+`;
+
+const Ment = styled.h1`
+  margin-top: 30px;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  color: #0288d1;
+  text-align: center;
+`;
+
+function NoticeTalk({ userId, socket, data, userName }) {
   const [chatData, setChatData] = useState(null);
 
   const GetChatApi = () => {
@@ -15,6 +104,7 @@ function NoticeTalk({ userId, socket, data }) {
   useEffect(() => {
     GetChatApi();
   }, []);
+  console.log(data);
 
   const CallChatApi = (ChatValue) => {
     axios.post(`http://localhost:8080/chat/insertOne`, {
@@ -26,7 +116,10 @@ function NoticeTalk({ userId, socket, data }) {
 
   const emitMessage = (ChatValue) => {
     return new Promise((resolve, reject) => {
-      socket.emit("send_message_notice", { message: ChatValue });
+      socket.emit("send_message_notice", {
+        userId: userId,
+        message: ChatValue,
+      });
     });
   };
 
@@ -38,45 +131,32 @@ function NoticeTalk({ userId, socket, data }) {
     event.currentTarget[0].value = ``;
   };
 
-  if (data !== null) {
-    const Ul = document.querySelector("ul");
-    const li = document.createElement("li");
-    Ul.appendChild(li);
-    li.innerHTML = `${userId}: ${data.message}`;
-  }
-
   return (
-    <div>
-      <h2>공지톡📢</h2>
+    <Container>
       {chatData === null ? (
         <div>
           <h3>"로딩중"</h3>
-          <ul></ul>
+          <ChatList></ChatList>
         </div>
       ) : (
-        <div
-          style={{
-            height: "300px",
-            overflow: "auto",
-            flexDirection: "column-reverse",
-            display: "flex",
-          }}
-        >
-          <form onSubmit={sendMessage}>
+        <ChatContainer>
+          <InputContainer onSubmit={sendMessage}>
             <input placeholder="채팅" />
             <button>보내기</button>
-          </form>
+          </InputContainer>
 
-          <ul>
+          <ChatList>
             {chatData === null
               ? null
               : chatData.map((data, index) => (
                   <li key={index}>{`${data.userId}: ${data.chatData}`}</li>
                 ))}
-          </ul>
-        </div>
+          </ChatList>
+        </ChatContainer>
       )}
-    </div>
+      <Ment>바른 채팅</Ment>
+    </Container>
   );
 }
+
 export default NoticeTalk;
