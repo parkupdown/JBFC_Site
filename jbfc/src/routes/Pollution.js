6453395478cb1eab8e7bfda736d5248c76e7f9a2
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-04-12 13:37:59 +0900

    refactor: Api 데이터가 부정확하여 다른 APi로 교체

--- jbfc/src/routes/Pollution.js
+++ jbfc/src/routes/Pollution.js
@@ -11,33 +11,37 @@ function UnixToDate(Unix) {
 } //옮겨줘여함
 
 function Pollution({ data }) {
-  const makePollutionInfo = (data) => {
+  console.log(data);
+
+  const pollutionInfo = data.data.response.body.items[7];
+
+  const pm10 = parseInt(pollutionInfo.pm10Value);
+  //미세먼지
+  const pm25 = parseInt(pollutionInfo.pm25Value);
+  //초미세먼지
+  const pm10Grade = pollutionInfo.pm10Grade;
+  //미세먼지 등급
+  const pm25Grade = pollutionInfo.pm25Grade;
+  //초미세먼지 등급
+
+  const checkPollutionCondition = () => {
     let message;
     let iconClassName;
 
-    const pollutionInfo = data.data.list;
-
-    const pm10 = pollutionInfo[0].components.pm10;
-    //미세먼지
-    const pm25 = pollutionInfo[0].components.pm2_5;
-    //초미세먼지
-    const day = UnixToDate(pollutionInfo[0].dt);
-
-    if (pm10 <= 30 || pm25 <= 15) {
-      message = Constants.POLLUTION.VERYGOOD;
-      iconClassName = Constants.POLLUTIONICON.VERYGOOD;
-    } else if (pm10 <= 80 || pm25 <= 35) {
-      message = Constants.POLLUTION.GOOD;
-      iconClassName = Constants.POLLUTIONICON.GOOD;
-    } else if (pm10 <= 150 || pm25 <= 75) {
-      message = Constants.POLLUTION.BAD;
-      iconClassName = Constants.POLLUTIONICON.BAD;
-    } else {
+    if (pm10Grade === "4" || pm25Grade === "4") {
       message = Constants.POLLUTION.VERYBAD;
       iconClassName = Constants.POLLUTIONICON.VERYBAD;
+    } else if (pm10Grade === "3" || pm25Grade === "3") {
+      message = Constants.POLLUTION.BAD;
+      iconClassName = Constants.POLLUTIONICON.BAD;
+    } else if (pm10Grade === "2" || pm25Grade === "2") {
+      message = Constants.POLLUTION.GOOD;
+      iconClassName = Constants.POLLUTIONICON.GOOD;
+    } else if (pm10Grade === "1" || pm25Grade === "1") {
+      message = Constants.POLLUTION.VERYGOOD;
+      iconClassName = Constants.POLLUTIONICON.VERYGOOD;
     }
-
-    return [pm10, pm25, day, message, iconClassName];
+    return [message, iconClassName];
   };
 
   const PollutionWrapper = styled.div`
@@ -78,19 +82,12 @@ function Pollution({ data }) {
 
   return (
     <PollutionWrapper>
-      <PollutionHeader>실시간 내 위치 미세먼지</PollutionHeader>
-      <Icon className={makePollutionInfo(data)[4]}></Icon>
+      <PollutionHeader>실시간 광주 북구 미세먼지</PollutionHeader>
+      <Icon className={checkPollutionCondition()[1]}></Icon>
       <PollutionList>
-        <PollutionListItem>{makePollutionInfo(data)[3]}</PollutionListItem>
-        <PollutionListItem>
-          미세먼지: {makePollutionInfo(data)[0]} pm
-        </PollutionListItem>
-        <PollutionListItem>
-          초미세먼지: {makePollutionInfo(data)[1]} pm
-        </PollutionListItem>
-        <PollutionListItem>
-          측정기준: {makePollutionInfo(data)[2]}
-        </PollutionListItem>
+        <PollutionListItem>{checkPollutionCondition()[0]}</PollutionListItem>
+        <PollutionListItem>미세먼지: {pm10} ㎍/㎥</PollutionListItem>
+        <PollutionListItem>초미세먼지: {pm25} ㎍/㎥</PollutionListItem>
       </PollutionList>
     </PollutionWrapper>
   );
