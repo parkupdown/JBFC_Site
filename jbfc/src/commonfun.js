7aaa203bf3c6641596e2e8c9af20501dbb7d1874
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-04-18 01:42:25 +0900

    Update commonfun.js

--- jbfc/src/commonfun.js
+++ jbfc/src/commonfun.js
@@ -3,7 +3,7 @@ const Common = {
     const time = new Date(Unix * 1000);
     const week = [`일`, `월`, `화`, `수`, `목`, `금`, `토`];
     const dayOfWeek = week[time.getDay()];
-    return `${time.getMonth() + 1}월 ${time.getDate()}일 ${dayOfWeek}요일`;
+    return `${time.getMonth() + 1}월 ${time.getDate()}일 ${dayOfWeek}요일 `;
   }, //옮겨줘여함
 };
 export default Common;
