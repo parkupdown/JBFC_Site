fb0ba5abb76ff437a40974f0970760a2ac5ea370
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-04-19 18:12:27 +0900

    fix: 같은 도메인만 받도록하는 header 모든 제약 해제

--- jbfc/server.js
+++ jbfc/server.js
@@ -24,12 +24,6 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Cross-Origin-Embedder-Policy", "credentialless");
-
   next();
 });
 app.use(
