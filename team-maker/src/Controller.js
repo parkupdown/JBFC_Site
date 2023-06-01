5c752f1ec4e90c1e8f47f3f1528e5dfcff0c1742
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-06-01 23:04:28 +0900

    refactor: 입력값에 제한을 둠

--- team-maker/src/Controller.js
+++ team-maker/src/Controller.js
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import Inputview from "./InputView";
+import PlayerBoxMaker from "./PlayerBoxMaker";
 
 export default function Controller() {
   const [players, setPlayers] = useState(``);
   const [teams, setTeams] = useState(``);
+  console.log(players, teams);
   return (
     <div>
-      <Inputview setPlayers={setPlayers} setTeams={setTeams} />
+      <Inputview
+        setPlayers={setPlayers}
+        players={players}
+        setTeams={setTeams}
+        teams={teams}
+      />
+      <PlayerBoxMaker players={players} teams={teams} />
     </div>
   );
 }
--- team-maker/src/InputView.js
+++ team-maker/src/InputView.js
@@ -1,12 +1,26 @@
-export default function Inputview({ setPlayers, setTeams }) {
+export default function Inputview({ setPlayers, players, setTeams, teams }) {
   // class로 묶어서 바로 input이 생기면 좋을듯
-  const onChangePeople = (event) => {
+  const onChangePlayer = (event) => {
     event.preventDefault();
-    setPlayers(event.currentTarget.value);
+    const playerNum = event.currentTarget.value;
+    setPlayers(playerNum);
+    if (playerNum > 40) {
+      setPlayers(40);
+    }
+    if (playerNum < 0) {
+      setPlayers(0);
+    }
   };
   const onChangeTeam = (event) => {
     event.preventDefault();
-    setTeams(event.currentTarget.value);
+    const teamNum = event.currentTarget.value;
+    setTeams(teamNum);
+    if (teamNum > 10) {
+      setTeams(10);
+    }
+    if (teamNum < 0) {
+      setTeams(0);
+    }
   };
 
   return (
@@ -14,13 +28,15 @@ export default function Inputview({ setPlayers, setTeams }) {
       <form>
         <input
           placeholder="총 몇명인가요?"
-          onChange={onChangePeople}
+          onChange={onChangePlayer}
           type="number"
+          value={players}
         />
         <input
           placeholder="몇 팀으로 나눌 건가요?"
           onChange={onChangeTeam}
           type="number"
+          value={teams}
         ></input>
       </form>
     </div>
