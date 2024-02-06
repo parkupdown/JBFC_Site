5433a7275214a225c1c56f9ec275cc71397181ac
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-02-06 14:07:27 +0900

    feat: Main 컴포넌트 개발 (Swiper)

--- client/src/Main/Main.jsx
+++ client/src/Main/Main.jsx
@@ -3,9 +3,54 @@ import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import img1 from "./Poster/1.png";
+import img2 from "./Poster/2.png";
+import img3 from "./Poster/3.png";
+import img4 from "./Poster/4.jpeg";
+import player1 from "./Player/1.png";
+import player2 from "./Player/2.png";
+import player3 from "./Player/3.png";
+import player4 from "./Player/4.png";
+import player5 from "./Player/5.png";
 
+const Container = styled.div`
+  h1 {
+    font-weight: 300;
+    text-align: center;
+    margin-top: 20px;
+  }
+  span {
+    font-weight: 300;
+    text-align: left;
+    margin-left: 5px;
+    display: block;
+    margin-top: 20px;
+  }
+`;
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 30px;
+`;
+const SwipterContainer = styled.div`
+  margin-top: 30px;
+  padding: 20px;
+  text-align: center;
+  img {
+    border-radius: 10px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 export default function Main() {
   // 여기서 jwt여부 체크해서 없으면 바로 그냥 로그인으로
+  const playerArr = [player1, player2, player3, player4, player5];
   const navigate = useNavigate();
   const goLogin = () => {
     navigate("/login");
@@ -37,9 +82,61 @@ export default function Main() {
   }, []);
 
   return (
-    <div>
-      <h1>메인입니다.</h1>
-      <h2>주인은 {userId}</h2>
-    </div>
+    <Container>
+      <h1>짝발란스</h1>
+      <NavContainer>
+        <div>팀</div>
+        <div>게시판</div>
+        <div>경기일정</div>
+        <div>피드백</div>
+      </NavContainer>
+      <span>선수단 소개</span>
+      <SwipterContainer>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={2}
+          loop={true}
+          modules={[Navigation, Autoplay]}
+          autoplay={{ delay: 2000 }}
+        >
+          {playerArr.map((player, index) => (
+            <SwiperSlide key={index}>
+              <img src={player} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </SwipterContainer>
+
+      <SwipterContainer>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          loop={true}
+          modules={[Navigation]}
+          navigation
+        >
+          <SwiperSlide>
+            <a href="https://m.sports.naver.com/video/1145409">
+              <img src={img1} />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://m.sports.naver.com/video/1145598">
+              <img src={img2} />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://m.sports.naver.com/video/1145464">
+              <img src={img3} />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://m.sports.naver.com/video/1145375">
+              <img src={img4} />
+            </a>
+          </SwiperSlide>
+        </Swiper>
+      </SwipterContainer>
+    </Container>
   );
 }
