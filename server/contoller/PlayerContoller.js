c3ae284834b5a6558b6131c0a70410864e9fa98f
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-04-16 19:48:46 +0900

    feat: [server] getPlayersData api 개발

--- server/contoller/PlayerContoller.js
+++ server/contoller/PlayerContoller.js
@@ -96,9 +96,40 @@ const getPlayerData = (req, res) => {
   });
 };
 
+// api호출은 각 month마다 1번씩만
+
+const getPlayersData = (req, res) => {
+  if (!isAuthorization(req, res)) {
+    return;
+  }
+
+  let { schedule_id } = req.query;
+  schedule_id = schedule_id.split(`,`);
+
+  let sql = "SELECT * FROM player WHERE schedule_id IN (?)";
+  conn.query(sql, [schedule_id], (err, results) => {
+    if (err) {
+      res.status(StatusCodes.BAD_REQUEST).end();
+      return;
+    }
+    const groupedResults = schedule_id.map((id) => {
+      // 현재 schedule_id에 해당하는 결과만 필터링
+      const filteredResults = results.filter(
+        (result) => result.schedule_id == id
+      );
+      return filteredResults;
+    });
+
+    res.status(StatusCodes.OK).json(groupedResults);
+
+    return;
+  });
+};
+
 module.exports = {
   insertPlayerData,
   getPlayerData,
   updatePlayerScore,
   deletePlayerScore,
+  getPlayersData,
 };
